ab48e15b9e04cf7f23e7701dfcb12613f80621b4
Ali Afroozeh <afroozeh@gmail.com>
2018-09-14 01:37:07 +0200

    Update plot.sh

--- plot.sh
+++ plot.sh
@@ -1,16 +1,25 @@
 #!/usr/local/bin/Rscript
 
-iguana <- read.csv("Iguana.csv", header=TRUE, sep=",", dec=",", stringsAsFactors=FALSE)
-iguana[, "Score"]  <- as.numeric(iguana[, "Score"])
+Sys.setenv(LANG = "en")
 
-antlr <- read.csv("Antlr.csv", header=TRUE, sep=",", dec=",", stringsAsFactors=FALSE)
-antlr[, "Score"]  <- as.numeric(antlr[, "Score"])
+args<-commandArgs(TRUE)
 
+names <- c("RxJava", "elasticsearch", "jdk7u-jdk", "guava", "junit4")
 
-pdf("plot.pdf", width=12, height=8)
+for (name in names) {
 
-boxplot(iguana$Score, antlr$Score, horizontal=TRUE, names = c("Iguana", "Antlr"), las=2, outline=FALSE)
-text(x = boxplot.stats(round(iguana$Score, 2))$stats, labels = boxplot.stats(round(iguana$Score, 2))$stats, y = 0.55)
-text(x = boxplot.stats(round(antlr$Score, 2))$stats, labels = boxplot.stats(round(antlr$Score, 2))$stats, y = 1.55)
+	iguana <- read.csv(paste("Iguana_", name, ".csv", sep=""), header=TRUE, sep=",", dec=",", stringsAsFactors=FALSE)
+	iguana[, "Score"]  <- as.numeric(iguana[, "Score"])
+
+	antlr <- read.csv(paste("Antlr_", name, ".csv", sep=""), header=TRUE, sep=",", dec=",", stringsAsFactors=FALSE)
+	antlr[, "Score"]  <- as.numeric(antlr[, "Score"])
+
+	pdf(paste(name, ".pdf", sep=""), width=12, height=8)
+
+	boxplot(iguana$Score, antlr$Score, horizontal=TRUE, names = c("Iguana", "Antlr"), las=2, outline=FALSE)
+	text(x = boxplot.stats(round(iguana$Score, 2))$stats, labels = boxplot.stats(round(iguana$Score, 2))$stats, y = 0.55)
+	text(x = boxplot.stats(round(antlr$Score, 2))$stats, labels = boxplot.stats(round(antlr$Score, 2))$stats, y = 1.55)
+
+}
 
 dev.off()
