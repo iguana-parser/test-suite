f03e9ea24881a59642e74af25b8b5a49fcd71053
Ali Afroozeh <afroozeh@gmail.com>
2018-09-14 17:07:43 +0200

    Update plot.sh to create total

--- plot.sh
+++ plot.sh
@@ -3,19 +3,9 @@
 library(extrafont)
 loadfonts()
 
-par(mar = c(4.2,4.2,0.2,0.2))
-
-names <- c("RxJava", "elasticsearch", "jdk7u-jdk", "guava", "junit4")
-
-for (name in names) {
+draw_plot <- function(name, iguana, antlr) {
 	output <- paste(name, ".pdf", sep="")
-	pdf(output, width=12, height=8, family="CM Roman")
-
-	iguana <- read.csv(paste("Iguana_", name, ".csv", sep=""), header=TRUE, sep=",", dec=",", stringsAsFactors=FALSE)
-	iguana[, "Score"]  <- as.numeric(iguana[, "Score"])
-
-	antlr <- read.csv(paste("Antlr_", name, ".csv", sep=""), header=TRUE, sep=",", dec=",", stringsAsFactors=FALSE)
-	antlr[, "Score"]  <- as.numeric(antlr[, "Score"])
+	pdf(output, width=12, height=6, family="CM Roman")
 
 	boxplot(iguana$Score, antlr$Score, horizontal=TRUE, names = c("Iguana", "ANTLR"), las=2, outline=FALSE)
 	text(x = boxplot.stats(round(iguana$Score, 2))$stats, labels = boxplot.stats(round(iguana$Score, 2))$stats, y = 0.55)
@@ -24,8 +14,34 @@ for (name in names) {
 	dev.off()
 }
 
+par(mar = c(4.2,4.2,0.2,0.2))
+
+names <- c("RxJava", "elasticsearch", "jdk7u-jdk", "guava", "junit4")
+
+total_iguana <- data.frame()
+total_antlr <- data.frame()
+
+for (name in names) {
+	output <- paste(name, ".pdf", sep="")
+	pdf(output, width=12, height=8, family="CM Roman")
+
+	iguana <- read.csv(paste("Iguana_", name, ".csv", sep=""), header=TRUE, sep=",", dec=",", stringsAsFactors=FALSE)
+	iguana[, "Score"]  <- as.numeric(iguana[, "Score"])
+	total_iguana <- rbind(total_iguana, iguana)
+
+	antlr <- read.csv(paste("Antlr_", name, ".csv", sep=""), header=TRUE, sep=",", dec=",", stringsAsFactors=FALSE)
+	antlr[, "Score"]  <- as.numeric(antlr[, "Score"])
+	total_antlr <- rbind(total_antlr, antlr)
+
+	draw_plot(name, iguana, antlr)
+}
+
+draw_plot("total", total_iguana, total_antlr)
+
 for (name in names) {
 	output <- paste(name, ".pdf", sep="")
 	embedded_output <- paste("embedded_", output, sep="")
 	embed_fonts(output, outfile=embedded_output)
 }
+
+embed_fonts("total.pdf", outfile="embedded_total.pdf")
