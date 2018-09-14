04c3ff4b0c45512fad62353a3298a979c090a89c
Ali Afroozeh <afroozeh@gmail.com>
2018-09-14 12:08:37 +0200

    Update Plot.sh to embed CM fonts

--- plot.sh
+++ plot.sh
@@ -1,12 +1,15 @@
 #!/usr/local/bin/Rscript
 
-Sys.setenv(LANG = "en")
+library(extrafont)
+loadfonts()
 
-args<-commandArgs(TRUE)
+par(mar = c(4.2,4.2,0.2,0.2))
 
 names <- c("RxJava", "elasticsearch", "jdk7u-jdk", "guava", "junit4")
 
 for (name in names) {
+	output <- paste(name, ".pdf", sep="")
+	pdf(output, width=12, height=8, family="CM Roman")
 
 	iguana <- read.csv(paste("Iguana_", name, ".csv", sep=""), header=TRUE, sep=",", dec=",", stringsAsFactors=FALSE)
 	iguana[, "Score"]  <- as.numeric(iguana[, "Score"])
@@ -14,12 +17,15 @@ for (name in names) {
 	antlr <- read.csv(paste("Antlr_", name, ".csv", sep=""), header=TRUE, sep=",", dec=",", stringsAsFactors=FALSE)
 	antlr[, "Score"]  <- as.numeric(antlr[, "Score"])
 
-	pdf(paste(name, ".pdf", sep=""), width=12, height=8)
-
-	boxplot(iguana$Score, antlr$Score, horizontal=TRUE, names = c("Iguana", "Antlr"), las=2, outline=FALSE)
+	boxplot(iguana$Score, antlr$Score, horizontal=TRUE, names = c("Iguana", "ANTLR"), las=2, outline=FALSE)
 	text(x = boxplot.stats(round(iguana$Score, 2))$stats, labels = boxplot.stats(round(iguana$Score, 2))$stats, y = 0.55)
 	text(x = boxplot.stats(round(antlr$Score, 2))$stats, labels = boxplot.stats(round(antlr$Score, 2))$stats, y = 1.55)
 
+	dev.off()
 }
 
-dev.off()
+for (name in names) {
+	output <- paste(name, ".pdf", sep="")
+	embedded_output <- paste("embedded_", output, sep="")
+	embed_fonts(output, outfile=embedded_output)
+}
